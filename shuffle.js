575f21487f3bea9355eb77d046c5fd5455e86dce
Kristen Payne <victorycabbage@gmail.com>
2019-04-11 09:58:48 -0700

    completed shuffle for js

--- shuffle.js
+++ shuffle.js
@@ -1,8 +1,15 @@
 const random = (bottom,ceil) => {
+  // returns a random number between bottom and ceil, ceil excluded.
+  return Math.floor((Math.random() * ((ceil - 1) - bottom)) + bottom);
 }
 
 const shuffleInPlace = (array) => {
+  // returns the array with the elements shuffled.
   for(let i = 0; i < array.length; i++){
-    // TODO FINISH WHEN NOT EXHAUSTED
+    let holder = array[i];
+    let swap_index = random(i,array.length);
+    array[i] = array[swap_index];
+    array[swap_index] = holder;
   }
+  return array;
 }
